02a37082b4570b445c471afaa8e685a66697f080
Benedict Odoom <1008909o@student.gla.ac.uk>
2012-10-30 18:51:15 +0000

    various changes

--- plugin/javascript/fishfood.js
+++ plugin/javascript/fishfood.js
@@ -29,6 +29,14 @@ function openConfig() {
 }
 
 $(document).ready(function () {
+    $('body').fireExtensionEvent('requestData');
+    CrossriderAPI.bindExtensionEvent(document.body, 'receiveData', function (e, data) {
+        // Verify that data was returned
+        if (data) {
+            // For this purpose of this demo, simply display the returned data to the user
+            alert('Welcome back ' + data.username);
+        }
+    });
     $("#settings").click(function () {
         if($('div.config').height() == 0){
             $('div.config').animate({ height: '300px' }, "slow");
